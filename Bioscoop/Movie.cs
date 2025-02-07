a5379a2ef1445ca346d74f7a21ef884105afed9d
JanvElswijk <jajp.vanelswijk@student.avans.nl>
2025-02-07 12:25:05 +0100

    Implemented AddMovieScreening

--- Bioscoop/Movie.cs
+++ Bioscoop/Movie.cs
@@ -2,15 +2,15 @@
 
 public class Movie {
     private String _title;
+    private ICollection<MovieScreening> _screenings;
 
     public Movie(String title) {
         this._title = title;
+        this._screenings = new List<MovieScreening>();
     }
 
     public void AddScreening(MovieScreening movieScreening) {
-        //TODO: Implement
-        //new MovieScreening(this, LocalDateTime.now, 1.1)
-    }
+        this._screenings.Add(movieScreening); }
 
     public override String ToString() {
         return this._title;
